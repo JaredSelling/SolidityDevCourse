4c2c893a4e5f3133185dbd78b21ba4250fe09819
JaredSelling <selling.jared@gmail.com>
2021-12-23 12:47:31 -0500

    Add project files for React video project

---
+++ ReduxSimpleStarter/src/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyASLu4djxEQ-15KYjhJBhETvJngWvPZO_g';
+
+
+
+  // Create new compoent.  Should produce some html
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'gaming'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos}/>
+      </div>
+
+    );
+  }
+}
+
+// Put component's generated tml in the dom
+ReactDOM.render(<App />, document.querySelector('.container'));
