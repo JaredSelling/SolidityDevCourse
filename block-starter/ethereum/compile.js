53467399a29efe9c4167cd7c98171e235c3af87b
JaredSelling <selling.jared@gmail.com>
2021-12-30 08:24:46 -0500

    Add block-starter compile script

---
+++ block-starter/ethereum/compile.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const fs = require('fs-extra');
+const solc = require('solc');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fs.readFileSync(campaignPath, 'utf8');
+
+fs.ensureDirSync(buildPath);
+
+const input = {
+  language: 'Solidity',
+
+  sources: {
+    'Campaign.sol': {
+      content: source
+    }
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*']
+      },
+    },
+  },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Campaign.sol'];
+
+for(let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract + '.json'),
+    output[contract]
+  );
+}
