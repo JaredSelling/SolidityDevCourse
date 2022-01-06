a49ecd2246c5067669d037c7f0eb8aa0941c1353
JaredSelling <selling.jared@gmail.com>
2022-01-06 07:07:20 -0500

    Create factory.js to retrieve the same factory instance for other files

---
+++ block-starter/ethereum/factory.js
@@ -0,0 +1,7 @@
+// Create pre-configured factory instance that we can import and use in other files
+import web3 './web3';
+import CampaignFactory from './build/CampaignFactory.json';
+
+const factoryInstance = await new web3.eth.Contract(CampaignFactory.abi, '0x35A34F030333b892970Bb2FEa4C3Cf04eF48C004');
+
+export default factoryInstance;
