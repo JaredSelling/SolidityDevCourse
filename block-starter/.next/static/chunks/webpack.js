c1651803ba9457bc46ed1f4a05147e3e52d859ca
JaredSelling <selling.jared@gmail.com>
2022-01-10 08:21:45 -0500

    .next file changes for block-starter

--- block-starter/.next/static/chunks/webpack.js
+++ block-starter/.next/static/chunks/webpack.js
@@ -57,6 +57,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -131,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7aad24939539241a"; }
+/******/ 		__webpack_require__.h = function() { return "858fa9d2af33750c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
